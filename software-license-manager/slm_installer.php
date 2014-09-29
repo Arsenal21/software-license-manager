ed348a2c50de8b1969e7ad821fbd5b5401f31b92
amin0_000 <amin0_000@aminpc>
2014-09-29 11:06:24 +1000

    database charset and collate improvements

--- software-license-manager/slm_installer.php
+++ software-license-manager/slm_installer.php
@@ -1,11 +1,22 @@
 <?php
 //***** Installer *****
+global $wpdb;
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');	
 
 //***Installer variables***
 $lic_key_table = SLM_TBL_LICENSE_KEYS;
 $lic_domain_table = SLM_TBL_LIC_DOMAIN;
 
+$charset_collate = '';
+if (!empty($wpdb->charset)){
+    $charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+}else{
+    $charset_collate = "DEFAULT CHARSET=utf8";
+}
+if (!empty($wpdb->collate)){
+    $charset_collate .= " COLLATE $wpdb->collate";
+}
+        
 $lk_tbl_sql = "CREATE TABLE " . $lic_key_table . " (
       id int(12) NOT NULL auto_increment,
       license_key varchar(255) NOT NULL,
@@ -21,7 +32,7 @@ $lk_tbl_sql = "CREATE TABLE " . $lic_key_table . " (
       date_renewed date NOT NULL DEFAULT '0000-00-00',
       date_expiry date NOT NULL DEFAULT '0000-00-00',
       PRIMARY KEY (id)
-      )ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+      )" . $charset_collate . ";";
 dbDelta($lk_tbl_sql);
 
 $ld_tbl_sql = "CREATE TABLE " .$lic_domain_table. " (
@@ -31,7 +42,7 @@ $ld_tbl_sql = "CREATE TABLE " .$lic_domain_table. " (
       registered_domain text NOT NULL ,
       item_reference varchar(255) NOT NULL,
       PRIMARY KEY ( id )
-      )ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+      )" . $charset_collate . ";";
 dbDelta($ld_tbl_sql);
 
 update_option("wp_lic_mgr_db_version", WP_LICENSE_MANAGER_DB_VERSION);
