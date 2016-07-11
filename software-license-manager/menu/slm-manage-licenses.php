24932ca9331bb13d3237f6c66393c44fa031fed0
Jedi Knight <>
2016-07-11 15:11:53 +0600

    escaping a form field value.

--- software-license-manager/menu/slm-manage-licenses.php
+++ software-license-manager/menu/slm-manage-licenses.php
@@ -35,7 +35,7 @@ function wp_lic_mgr_manage_licenses_menu() {
             ?>
             <form id="tables-filter" method="get" onSubmit="return confirm('Are you sure you want to perform this bulk operation on the selected entries?');">
                 <!-- For plugins, we also need to ensure that the form posts back to our current page -->
-                <input type="hidden" name="page" value="<?php echo $_REQUEST['page']; ?>" />
+                <input type="hidden" name="page" value="<?php echo esc_attr($_REQUEST['page']); ?>" />
                 <!-- Now we can render the completed list table -->
                 <?php $license_list->display(); ?>
             </form>
