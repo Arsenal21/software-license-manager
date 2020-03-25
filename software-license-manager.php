d7ca1db5232e716a7dcf0f298396c17bc326aa8e
Michel Velis <michel@epikly.com>
2020-03-25 17:26:06 -0400

    Error - sets all licenses as Expired if they don't have Expiration date Sorting by ID not working properly thanks to @MechComp for the help

--- software-license-manager.php
+++ software-license-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Software License Manager
-Version: 5.2.0
+Version: 5.2.1
 Plugin URI: https://github.com/michelve/software-license-manager/
 Author: Michel Velis
 Author URI: https://www.epikly.com/
@@ -19,9 +19,9 @@ if (!defined('WPINC')) {
 global $wpdb, $slm_debug_logger;
 
 //Short name/slug "SLM" or "slm"
-define('SLM_VERSION',               '5.2.0');
-define('SLM_DB_VERSION',            '4.1.2');
-define('SLM_REWRITE_VERSION',       '2.3.0');
+define('SLM_VERSION',               '5.2.1');
+define('SLM_DB_VERSION',            '4.1.3');
+define('SLM_REWRITE_VERSION',       '2.3.1');
 define('SLM_FOLDER',                dirname(plugin_basename(__FILE__)));
 define('SLM_URL',                   plugins_url('' ,__FILE__));
 define('SLM_ASSETS_URL',            SLM_URL   . '/public/assets/');
