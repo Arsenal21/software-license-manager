bfd573a1e6b617a766e398a7e27e28bb4a3ffbd5
Michel Velis <mvelis90@gmail.com>
2020-09-14 10:02:13 -0400

    version 5.5.9

--- software-license-manager.php
+++ software-license-manager.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Software License Manager
-Version: 5.5.8
+Version: 5.5.9
 Plugin URI: https://github.com/michelve/software-license-manager/
 Author: Michel Velis
 Author URI: https://www.epikly.com/
@@ -19,9 +19,9 @@ if (!defined('WPINC')) {
 global $wpdb, $slm_debug_logger;
 
 //Short name/slug "SLM" or "slm"
-define('SLM_VERSION',               '5.5.8');
-define('SLM_DB_VERSION',            '4.3.0');
-define('SLM_REWRITE_VERSION',       '2.4.1');
+define('SLM_VERSION',               '5.5.9');
+define('SLM_DB_VERSION',            '4.3.1');
+define('SLM_REWRITE_VERSION',       '2.4.2');
 define('SLM_FOLDER',                dirname(plugin_basename(__FILE__)));
 define('SLM_URL',                   plugins_url('', __FILE__));
 define('SLM_ASSETS_URL',            SLM_URL   . '/public/assets/');
